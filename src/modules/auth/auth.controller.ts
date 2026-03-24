514cb248750e66ea11aed4d379bcae5f7165b764
matheeshaW <matheeshaweerakoon22@gmail.com>
2026-03-24 17:39:36 +0530

    feat: implement login functionality with jwt in AuthService and AuthController

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -10,4 +11,9 @@ export class AuthController {
   register(@Body() dto: RegisterDto) {
     return this.authService.register(dto);
   }
+
+  @Post('login')
+login(@Body() dto: LoginDto) {
+  return this.authService.login(dto);
+}
 }
