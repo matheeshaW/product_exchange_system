bda478c405234305efd9e0a8106323fb390a94f3
matheeshaW <matheeshaweerakoon22@gmail.com>
2026-03-24 18:05:32 +0530

    feat: add JWT authentication strategy and guard, and protect products route

--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { ProductsService } from './products.service';
 
 @Controller('products')
-export class ProductsController {}
+@UseGuards(JwtAuthGuard)
+export class ProductsController {
+constructor(private readonly productsService: ProductsService) {}
+
+ @Get()
+getProducts(@Req() req) {
+
+  const user = req.user;
+  console.log(user);
+}
+}
