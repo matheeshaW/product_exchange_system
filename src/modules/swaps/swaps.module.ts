158691832d7906a3e7667ec4108eb784f508f130
matheeshaW <matheeshaweerakoon22@gmail.com>
2026-03-30 13:31:25 +0530

    feat: owner id fetched for swaps

--- src/modules/swaps/swaps.module.ts
+++ src/modules/swaps/swaps.module.ts
@@ -3,9 +3,10 @@ import { SwapsService } from './swaps.service';
 import { SwapsController } from './swaps.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Swap } from './entities/swap.entity';
+import { Item } from '../items/entities/item.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Swap])],
+  imports: [TypeOrmModule.forFeature([Swap, Item])],
   providers: [SwapsService],
   controllers: [SwapsController]
 })
--- src/modules/swaps/swaps.service.ts
+++ src/modules/swaps/swaps.service.ts
@@ -9,33 +9,48 @@ import { Repository } from 'typeorm';
 import { Swap, SwapStatus } from './entities/swap.entity';
 import { CreateSwapDto } from './dto/create-swap.dto';
 import { UpdateSwapDto } from './dto/update-swap.dto';
+import { Item } from '../items/entities/item.entity';
 
 @Injectable()
 export class SwapsService {
   constructor(
     @InjectRepository(Swap)
     private readonly swapRepository: Repository<Swap>,
+
+    @InjectRepository(Item) 
+    private readonly itemRepository: Repository<Item>,
   ) {}
 
-  async createSwap(dto: CreateSwapDto, userId: string) {
-    try {
-      if (dto.requestedItemId === dto.offeredItemId) {
-        throw new BadRequestException('Cannot swap same item');
-      }
-
-      const swap = this.swapRepository.create({
-        requesterId: userId,
-        requestedItemId: dto.requestedItemId,
-        offeredItemId: dto.offeredItemId,
-        ownerId: '', // we fix this next
-      });
-
-      return this.swapRepository.save(swap);
-    } catch (error) {
-      if (error instanceof BadRequestException) throw error;
-      throw new InternalServerErrorException('Failed to create swap');
+async createSwap(dto: CreateSwapDto, userId: string) {
+  try {
+    if (dto.requestedItemId === dto.offeredItemId) {
+      throw new BadRequestException('Cannot swap same item');
     }
+
+    // GET THE REQUESTED ITEM FROM DB
+    const requestedItem = await this.itemRepository.findOne({
+      where: { id: dto.requestedItemId },
+    });
+
+    if (!requestedItem) {
+      throw new BadRequestException('Requested item not found');
+    }
+
+    //  CREATE SWAP WITH CORRECT ownerId
+    const swap = this.swapRepository.create({
+      requesterId: userId,
+      ownerId: requestedItem.ownerId, // owner id added
+      requestedItemId: dto.requestedItemId,
+      offeredItemId: dto.offeredItemId,
+    });
+
+    return this.swapRepository.save(swap);
+  } catch (error) {
+    if (error instanceof BadRequestException) throw error;
+
+    throw new InternalServerErrorException('Failed to create swap');
   }
+}
 
   async updateSwap(
     swapId: string,
