54195dc6a21e462af039250a97fa3aad05bf015c
matheeshaW <matheeshaweerakoon22@gmail.com>
2026-04-07 17:32:31 +0530

    feat: implement item filtering functionality with ItemFilters component

--- frontend/src/modules/items/ItemsPage.tsx
+++ frontend/src/modules/items/ItemsPage.tsx
@@ -1,28 +1,53 @@
 import { useEffect, useState } from 'react';
-import api from  '../../common/api/axios.instance';
-import type  { Item }  from './types/item.types';
+import api from '../../common/api/axios.instance';
+import type { Item } from './types/item.types';
 import type { ApiResponse } from '../../common/api/api.types';
 import ItemCard from './components/ItemCard';
+import ItemFilters from './components/ItemFilters';
 
-const ItemsPage  = () => {
+const ItemsPage = () => {
   const [items, setItems] = useState<Item[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
 
+  const [filters, setFilters] = useState({
+    search: '',
+    category: '',
+    condition: '',
+    district: '',
+    province: '',
+  });
+
+  const fetchItems = async (filtersData = filters) => {
+    try {
+      const query = new URLSearchParams();
+
+      Object.entries(filtersData).forEach(([key, value]) => {
+        if (value) {
+          query.append(key, value);
+        }
+      });
+
+      const res = await api.get<ApiResponse<Item[]>>(
+        `/items?${query.toString()}`
+      );
+
+      setItems(res.data.data);
+    } catch (error) {
+      console.error('Failed to fetch items', error);
+    }
+    finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
-    const fetchItems = async () => {
-      try {
-        const res = await api.get<ApiResponse<Item[]>>('/items');
-
-        setItems(res.data.data);
-      } catch (error) {
-        console.error('Failed to fetch items', error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
     fetchItems();
   }, []);
+
+  const handleFilterChange = (newFilters: typeof filters) => {
+    setFilters(newFilters);
+    fetchItems(newFilters);
+  };
 
   if (loading) {
     return <div className="p-4">Loading items...</div>;
@@ -33,6 +58,7 @@ const ItemsPage  = () => {
       <h1 className="text-2xl font-bold mb-4">
         Available Items
       </h1>
+      <ItemFilters onFilterChange={handleFilterChange} />
 
       {/* GRID */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
@@ -44,4 +70,4 @@ const ItemsPage  = () => {
   );
 };
 
-export default ItemsPage ;
+export default ItemsPage;
