c4277ee398a31826ce2fb44b42a8ee0861d9aa89
matheeshaW <matheeshaweerakoon22@gmail.com>
2026-04-09 12:00:00 +0530

    feat: add AuthGuard component to protect routes and ensure user authentication

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -4,15 +4,30 @@ import ItemsPage from '../modules/items/ItemsPage';
 import ItemDetailsPage from '../modules/items/ItemDetailsPage';
 import SwapsPage from '../modules/swaps/SwapsPage';
 import ChatPage from '../modules/chat/ChatPage';
+import AuthGuard from '../common/guards/AuthGuard';
 
 const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<ItemsPage />} />
+      <Route path="/" element={<AuthGuard><ItemsPage /></AuthGuard>} />
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/items/:id" element={<ItemDetailsPage />} />
-      <Route path="/swaps" element={<SwapsPage />} />
-      <Route path="/chat/:swapId" element={<ChatPage />} />
+      <Route path="/items/:id" element={<AuthGuard><ItemDetailsPage /></AuthGuard>} />
+      <Route
+        path="/swaps"
+        element={
+          <AuthGuard>
+            <SwapsPage />
+          </AuthGuard>
+        }
+      />
+      <Route
+        path="/chat/:swapId"
+        element={
+          <AuthGuard>
+            <ChatPage />
+          </AuthGuard>
+        }
+      />
     </Routes>
   );
 };
